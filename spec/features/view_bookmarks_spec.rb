e567f5ab2b58c04e1247aab99ff68cdd4816af2d
Arabella Knowles <arabellaknowles@hotmail.co.uk>
2020-10-22 15:14:06 +0100

    refactored feature tests to use .create method

--- spec/features/view_bookmarks_spec.rb
+++ spec/features/view_bookmarks_spec.rb
@@ -1,13 +1,12 @@
 require 'pg'
+require 'bookmarks'
 
 feature 'Viewing bookmarks' do
   scenario 'Visit bookmark page' do
-    connection = PG.connect(dbname: 'bookmark_manager_test')
-    
-    connection.exec("INSERT INTO bookmarks VALUES(1, 'http://www.makersacademy.com');")
-    connection.exec("INSERT INTO bookmarks VALUES(2, 'http://www.destroyallsoftware.com');")
-    connection.exec("INSERT INTO bookmarks VALUES(3, 'http://www.google.com');")
-    
+    Bookmark.create('http://www.makersacademy.com')
+    Bookmark.create('http://www.destroyallsoftware.com')
+    Bookmark.create('http://www.google.com')
+
     visit '/bookmarks'
 
     expect(page).to have_content "http://www.destroyallsoftware.com"
